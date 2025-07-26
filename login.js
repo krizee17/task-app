b9e2d1dbabf20c9f0ba1b515ab2b003e9d1e44b8
Kritika <akritika392@gmail.com>
2025-07-26 21:29:05 +0545

    Added login.js file for redirection

---
+++ login.js
@@ -0,0 +1,4 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevents the default form submission
+  window.location.href = 'index.html'; // Redirects to the dashboard
+}); 
