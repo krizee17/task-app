82e9ff6e3c53fcdfb69b7b38e98b2a115614b8bf
Kritika <akritika392@gmail.com>
2025-07-27 06:18:18 +0545

    Modified the login.js file

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
   event.preventDefault(); // Prevents the default form submission
-  window.location.href = 'index.html'; // Redirects to the dashboard
+  window.location.href = 'dashboard.html'; // Redirects to the dashboard
 }); 
