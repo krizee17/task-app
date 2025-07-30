5df2827035a7558ee83f10a4eed17f34c441f65d
Kritika <akritika392@gmail.com>
2025-07-30 21:47:28 +0545

    Modify login.js file

--- login.js
+++ login.js
@@ -1,4 +1,16 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevents the default form submission
-  window.location.href = 'dashboard.html'; // Redirects to the dashboard
-}); 
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevents the default form submission
+    window.location.href = "dashboard.html"; // Redirects to the dashboard
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const signupLink = document.querySelector(".signup-link");
+  if (signupLink) {
+    signupLink.addEventListener("click", function (e) {
+      e.preventDefault();
+      window.location.href = "../signup.html";
+    });
+  }
+});
